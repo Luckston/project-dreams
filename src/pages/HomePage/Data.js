0962a1b51d1507af48e680b02ec52dd7281325fa
Luckston <99181016+Luckston@users.noreply.github.com>
2022-03-01 11:03:11 +0100

    Add files via upload

---
+++ src/pages/HomePage/Data.js
@@ -0,0 +1,68 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Marketing Agency',
+    headline: 'Lead Generation Specialist for Online Businesses',
+    description:
+      'We help business owners increase their revenue. Our team of unique specialist can help you achieve your business goals.',
+    buttonLabel: 'Get Started',
+    imgStart: '',
+    img: require('../../images/undraw_segment_analysis_re_ocsl.svg').default,
+    alt: 'Image',
+    start: ''
+  };
+  
+  export const homeObjTwo = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Instant Setup',
+    headline: 'Extremely quick onboarding process',
+    description:
+      "Once you've joined, our team of specialist will reach out to you and get you set up in minutes.",
+    buttonLabel: 'Learn More',
+    imgStart: '',
+    img: require('../../images/lagos-techie-IgUR1iX0mqM-unsplash.jpg'),
+    alt: 'Image',
+    start: ''
+  };
+  
+  export const homeObjThree = {
+    primary: false,
+    lightBg: true,
+    lightTopLine: false,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Sarah Jeni',
+    headline:
+      'Ultra helped me increase my revenue by over 3X in less than 3 months!',
+    description:
+      "Their team is wonderful! I can't believe I didn't start working with them earlier.",
+    buttonLabel: 'View Case Study',
+    imgStart: 'start',
+    img: require('../../images/undraw_progress_data_re_rv4p.svg').default,
+    alt: 'Image',
+    start: 'true'
+  };
+  
+  export const homeObjFour = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Secure Database',
+    headline: 'All your data is stored on our secure server',
+    description:
+      'You will never have to worry about your information getting leaked. Our team of security experts will ensure your records are kept safe.',
+    buttonLabel: 'Sign Up Now',
+    imgStart: 'start',
+    img: require('../../images/undraw_secure_server_re_8wsq.svg').default,
+    alt: 'Image',
+    start: 'true'
+  };
